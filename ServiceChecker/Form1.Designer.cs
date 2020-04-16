a5fccd1f866df5d5973265b24d34159a8b602b11
Javad <jd.mn98@yahoo.com>
2020-04-16 16:00:53 +0430

    Add project files.

---
+++ ServiceChecker/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ServiceChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Light", 20F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(12, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stop Update Service";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 79);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI Light", 20F);
+            this.checkBox1.Location = new System.Drawing.Point(403, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 41);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "StartUp";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Light", 20F);
+            this.label2.Location = new System.Drawing.Point(0, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(556, 72);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Update Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ServiceChecker/Form1.cs
@@ -0,0 +1,176 @@
+﻿using IWshRuntimeLibrary;
+using ServiceChecker.Properties;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Management;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Windows.Forms;
+
+namespace ServiceChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ServiceController service = new ServiceController("Windows Update");
+        string path = "Win32_Service.Name='" + "wuauserv" + "'";
+        ManagementPath managementPath;
+        ManagementObject managementObject;
+        Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        Assembly curAssembly = Assembly.GetExecutingAssembly();
+
+        // Custom Functions
+        private void UpdateText(string MainText , string startMode , Color colorMain , Color startModeColor)
+        {
+            label1.ForeColor = colorMain;
+            label2.ForeColor = startModeColor;
+            label1.Text = MainText;
+            label2.Text = "StartMode is : " + startMode;
+        }
+        private void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = "Windows Update Checker Shortcut"; // The description of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Save();                                          // Save the shortcut
+        }
+
+        // System Functions
+        private void button_StartModeClick(object sender , EventArgs e)
+        {
+            if (managementObject["StartMode"].ToString() == "Auto" || managementObject["StartMode"].ToString() == "Manual")
+            {
+                object[] parameters = new object[1] { "Disabled" };
+                managementObject.InvokeMethod("ChangeStartMode", parameters);
+                managementObject = new ManagementObject(managementPath);
+                UpdateText("Good , Windows Update has been Stopped", managementObject["StartMode"].ToString(), Color.Green, Color.Green);
+                button1.Enabled = false;
+                button1.Text = "Disabled";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+                if (managementObject["StartMode"].ToString() == "Auto" || managementObject["StartMode"].ToString() == "Manual")
+                {
+                    object[] parameters = new object[1] { "Disabled" };
+                    managementObject.InvokeMethod("ChangeStartMode", parameters);
+                    managementObject = new ManagementObject(managementPath);
+                }
+                UpdateText("Good , Windows Update has been Stopped", managementObject["StartMode"].ToString(), Color.Green , Color.Green);
+                button1.Enabled = false;
+                button1.Text = "Disabled";
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //List<string> list = ServiceController.GetServices().Select(a => a.DisplayName).ToList
+            // Create Shortcut
+            if (!System.IO.File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),curAssembly.GetName().Name +".lnk")))
+            {
+                CreateShortcut(curAssembly.GetName().Name, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), curAssembly.Location);
+            }
+
+            service.MachineName = Environment.MachineName;
+            managementPath = new ManagementPath(path);
+            managementObject = new ManagementObject(managementPath);
+            // Start Up
+            if (key.GetValue(curAssembly.GetName().Name) != null)
+            {
+                if (key.GetValue(curAssembly.GetName().Name).ToString() != curAssembly.Location)
+                {
+                    key.SetValue(curAssembly.GetName().Name, curAssembly.Location);
+                }
+                Settings.Default.StartUp = true;
+                Settings.Default.Save();
+                checkBox1.CheckState = CheckState.Checked;
+            }
+            else
+            {
+                Settings.Default.StartUp = false;
+                Settings.Default.Save();
+                checkBox1.CheckState = CheckState.Unchecked;
+            }
+
+            // Check Service
+            if (service.Status == ServiceControllerStatus.Running)
+            {
+                UpdateText("Windows Update is Running", managementObject["StartMode"].ToString(), Color.Red , Color.Red);
+                button1.Enabled = true;
+            }
+            else if (service.Status == ServiceControllerStatus.Stopped)
+            {
+                if (managementObject["StartMode"].ToString() != "Disabled")
+                {
+                    UpdateText("Good , Windows Update has been Stopped",managementObject["StartMode"].ToString(), Color.Green , Color.Red);
+                    button1.Text = "Change StartMode to Disabled";
+                    button1.Click -= button1_Click;
+                    button1.Click += button_StartModeClick;
+                }
+                else
+                {
+                    UpdateText("Good , Windows Update has been Stopped", managementObject["StartMode"].ToString(), Color.Green, Color.Green);
+                    button1.Enabled = false;
+                    button1.Text = "Disabled";
+                }
+            }
+            else
+            {
+                UpdateText("ReRun the App","", Color.Gray,Color.Gray);
+                button1.Enabled = false;
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!Settings.Default.StartUp)
+            {
+                try
+                {
+                    key.SetValue(curAssembly.GetName().Name, curAssembly.Location);
+                    Settings.Default.StartUp = true;
+                    Settings.Default.Save();
+                    MessageBox.Show("Program Added To StartUp Successfully :D","StartUp",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                }
+            }
+            else if (Settings.Default.StartUp && !checkBox1.Checked)
+            {
+                try
+                {
+                    key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    key.DeleteValue(curAssembly.GetName().Name);
+                    Settings.Default.StartUp = false;
+                    Settings.Default.Save();
+                    MessageBox.Show("Program Removed From StartUp Successfully :D", "StartUp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                }
+            }
+        }
+    }
+}
